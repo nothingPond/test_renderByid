e13c663e996cf3701f5154e4346dadb76a91f687
Pondz <142286063+chairat25@users.noreply.github.com>
2025-04-24 00:41:06 +0700

    test checkbox

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,43 @@
-import { Grid2, Typography } from "@mui/material";
+import { Checkbox, Grid2, Typography } from "@mui/material";
+import { useState } from "react";
 
-const Home = () => {
+function Home() {
+  const [checked, setChecked] = useState(false);
+
   return (
-    <Grid2>
-      <Typography > home</Typography>
-    </Grid2>
+    <>
+      <Grid2>
+        <Typography> home</Typography>
+      </Grid2>
+      <Grid2>
+        <Checkbox
+          title="Checkbox"
+          id="checkbox"
+          name="checkbox"
+          value={checked}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setChecked(e.target.checked);
+          }}
+        ></Checkbox>
+      </Grid2>
+    </>
   );
-};
+}
 
 export default Home;
+
+//
+//
+
+// export function Home = () => {
+
+//   return (
+
+//     <>
+//
+//     </>
+//   );
+// };
+
+// export default Home;
